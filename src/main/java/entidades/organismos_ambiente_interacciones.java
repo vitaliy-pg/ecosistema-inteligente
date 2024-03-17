9f347e8470782ed2c509d04fc48cf9d2e0c01cdc
vitaliy-pg <aljndrvt@myuax.com>
2024-03-17 13:01:36 +0100

    motodo para que los animales puedan moverse

--- src/main/java/entidades/organismos_ambiente_interacciones.java
+++ src/main/java/entidades/organismos_ambiente_interacciones.java
@@ -37,3 +37,8 @@ class Animal extends Organismo {
     public Animal(int posX, int posY) {
         super(posX, posY);
     }
+    public void interactuar(Ambiente ambiente) {
+        // Los animales pueden moverse, buscar comida, depredar, reproducirse, etc.
+        // Aquí podrías implementar la lógica específica para los animales
+    }
+}

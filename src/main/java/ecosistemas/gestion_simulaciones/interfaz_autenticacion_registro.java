48cc50179bbcc019233da988220ee4797f908146
vitaliy-pg <aljndrvt@myuax.com>
2024-03-17 13:36:51 +0100

    metodo para verificar usuarios

--- src/main/java/ecosistemas/gestion_simulaciones/interfaz_autenticacion_registro.java
+++ src/main/java/ecosistemas/gestion_simulaciones/interfaz_autenticacion_registro.java
@@ -27,3 +27,8 @@ class Autenticacion {
     public void registrarUsuario(String nombreUsuario, String contrasena) {
         usuarios.put(nombreUsuario, new Usuario(nombreUsuario, contrasena));
     }
+    public boolean autenticarUsuario(String nombreUsuario, String contrasena) {
+        Usuario usuario = usuarios.get(nombreUsuario);
+        return usuario != null && usuario.verificarContrasena(contrasena);
+    }
+}

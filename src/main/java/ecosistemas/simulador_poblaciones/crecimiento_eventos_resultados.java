27b4b28177d99f1a2e9409418be627af6e0389a7
vitaliy-pg <aljndrvt@myuax.com>
2024-03-17 13:14:23 +0100

    creo la clase especie

--- src/main/java/ecosistemas/simulador_poblaciones/crecimiento_eventos_resultados.java
+++ src/main/java/ecosistemas/simulador_poblaciones/crecimiento_eventos_resultados.java
@@ -1,3 +1,19 @@
 package ecosistemas.simulador_poblaciones;
 import java.util.ArrayList;
 import java.util.Random;
+class Especie {
+    private String nombre;
+    private int poblacion;
+
+    public Especie(String nombre, int poblacionInicial) {
+        this.nombre = nombre;
+        this.poblacion = poblacionInicial;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getPoblacion() {
+        return poblacion;
+    }

b1e3eee8773505f80a17808d3cdf71e56e650cef
vitaliy-pg <aljndrvt@myuax.com>
2024-03-17 12:56:59 +0100

    creo la clase organismo

--- src/main/java/entidades/organismos_ambiente_interacciones.java
+++ src/main/java/entidades/organismos_ambiente_interacciones.java
@@ -1,3 +1,19 @@
 package entidades;
 import java.util.ArrayList;
 import java.util.Random;
+abstract class Organismo {
+    protected int posicionX;
+    protected int posicionY;
+    protected int salud;
+    protected int edad;
+    protected boolean estadoReproductivo;
+
+    public Organismo(int posX, int posY) {
+        this.posicionX = posX;
+        this.posicionY = posY;
+        this.salud = 100; // Salud inicial
+        this.edad = 0; // Edad inicial
+        this.estadoReproductivo = false; // No reproductivo al inicio
+    }
+    }
+

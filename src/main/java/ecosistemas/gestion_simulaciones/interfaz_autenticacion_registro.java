799a65438e7c00407f887aa3c923ea5f7b5b4e3c
vitaliy-pg <aljndrvt@myuax.com>
2024-03-17 13:35:01 +0100

    creo la clase usuario

--- src/main/java/ecosistemas/gestion_simulaciones/interfaz_autenticacion_registro.java
+++ src/main/java/ecosistemas/gestion_simulaciones/interfaz_autenticacion_registro.java
@@ -2,3 +2,15 @@ package ecosistemas.gestion_simulaciones;
 import java.util.HashMap;
 import java.util.Map;
 
+class Usuario {
+    private String nombreUsuario;
+    private String contrasena;
+
+    public Usuario(String nombreUsuario, String contrasena) {
+        this.nombreUsuario = nombreUsuario;
+        this.contrasena = contrasena;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
